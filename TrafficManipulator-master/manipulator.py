f7c6beac7a8ff3af079968a8195d005864b85fb4
Rising Apivotto <alessiapivotto1@gmail.com>
2025-10-09 13:34:18 +0000

    minor fexes

--- TrafficManipulator-master/manipulator.py
+++ TrafficManipulator-master/manipulator.py
@@ -258,7 +258,7 @@
 
 
 
-# # python main.py -m example/test.pcap -b example/mimic_set.npy -n example/normalizer.pkl -i example/init.pcap
+# # python3 main.py -m example/test.pcap -b example/mimic_set.npy -n example/normalizer.pkl -i example/init.pcap
 
 
 
